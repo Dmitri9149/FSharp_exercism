66c62d4dbc35ed2dde8ef54bad29c63cc17b77fd
Dmitri9149 <me@notset.com>
2022-02-07 12:13:12 +0200

    Queenattack

---
+++ QueenAttack.fs
@@ -0,0 +1,7 @@
+
+let create (position: int * int) = if fst position = snd position then true else false
+let canAttack (queen1: int * int) (queen2: int * int) = 
+    match queen1 , queen2 with 
+        | (x , y) , (z , v) when x = z || y = v -> true 
+        | (x , y) , (z , v) when abs (x - z)  =  abs (y - v) -> true
+        | _ -> false 
