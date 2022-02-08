d8d12ded02856ca56c5fc341c71bbe81a7dd29cb
Dmitri9149 <me@notset.com>
2022-02-08 17:44:18 +0200

    some addition

--- Accumulate.fs
+++ Accumulate.fs
@@ -2,3 +2,11 @@ module Accumulate
 
 let accumulate (func: 'a -> 'b) (input: 'a list): 'b list = 
     Seq.toList ( seq { for d in input do yield (func d)} )
+
+(*
+let rec accumulate f lst = 
+    match lst with
+    | [] -> []
+    | x::xs -> f x :: accumulate f xs
+
+*)   
